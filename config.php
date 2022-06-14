89b6488873d51d22102286574b70946c96012645
Nguyễn Quang Huy <huynq@isi.com.vn>
2022-06-15 06:29:36 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$connection=mysqli_connect ("localhost", 'huyremy', '','không ai có thể biết được điền cái gì vào đây');
+if (!$connection) {
+    die('Not connected : ' . mysqli_connect_error());
+}
+?>
